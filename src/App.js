05544a0f77674ee6fef9ac390b5f2414aebbd4f7
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-21 21:25:31 -0300

    feat: Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header'
 import './App.css';
 
 const App = () => {
   return (
     <div className="container">
-      Hello
+      <Header/>
     </div>
   );
 }
