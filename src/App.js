8fe1e4cd6ca67c325b37333563127820d9111dfb
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-21 21:40:56 -0300

    feat: conexao com API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './services/api'
 import Header from './components/Header'
 import './App.css';
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await api.get(`/characters`)
+
+      console.log(result.data)
+    }
+
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
       <Header/>
